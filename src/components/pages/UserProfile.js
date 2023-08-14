d5b9a5e56e5f61b8e1752b493b718ade31ad7cc0
Amanda <jonesamanda996@gmail.com>
2023-08-14 18:59:21 -0400

    cancel subscription button

--- src/components/pages/UserProfile.js
+++ src/components/pages/UserProfile.js
@@ -39,22 +39,35 @@ function UserProfile() {
             // Set total orders in the Redux store
             dispatch(setTotalOrders(userWithOrders.orders.length));
 
+        
+
         } catch (error) {
             console.error("Error fetching user orders:", error.message);
         }
     };
-
-    const handleLogout = () => {
+    const handleDeleteOrder = async (orderId) => {
+        try {
+            console.log(orderId)
+            const response = await axios.delete(`http://127.0.0.1:8080/orders/${orderId}`);
+            if (response.status === 204) {
+            // Order deleted successfully, update the user's orders in the store
+            const updatedOrders = user.orders.filter((order) => order.orderId !== orderId);
+            dispatch(updateUserOrders(updatedOrders));
+            dispatch(setTotalOrders(updatedOrders.length));
+            }
+        } catch (error) {
+            console.error("Error deleting order:", error.message);
+        }
+    }
+        const handleLogout = () => {
         dispatch(logout());
     };
 
     if (!isAuthenticated || !user) {
-        // Loading or not authenticated
         // navigate("/");
         return null;
-        // return <p>Please log in to access this page.</p>;
     }
-    // console.log("users order", user.order)
+
     return (
         <div>
             <p>Welcome, {user.email}!</p>
@@ -65,11 +78,12 @@ function UserProfile() {
                     <ul>
                         {user.orders.map((order, index) => (
                             <li key={order.orderId}>
-                                <p>Order Number: {index + 1}</p> 
+                                <p>Subscription Number: {index + 1}</p> 
                                 <p>Restaurant: {order.restaurant}</p>
                                 <p>Food Order: {order.foodOrder}</p>
                                 <p>Delivery Frequency: {order.deliveryFrequency}</p>
                                 <p>Total Cost {order.deliveryFrequency}: ${order.totalAmount}</p>
+                                <button onClick={() => handleDeleteOrder(order.orderId)}>Cancel Subscription</button>
                             </li>
                         ))}
                     </ul>
